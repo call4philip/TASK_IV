c5195d29b48b5ab593a48d794e540c8355a5328d
call4philip <philiparthur.cinco0075@gmail.com>
2025-11-28 15:51:59 +0800

    Update Module_repository.dart

--- lib/domain/usecases/Module_repository.dart
+++ lib/domain/usecases/Module_repository.dart
@@ -1,1 +1,15 @@
+import 'package:dartz/dartz.dart';
+import '../entities/module.dart';
+import '../failures/failure.dart';
+import '../repositories/module_repository.dart';
+
+class GetModuleById {
+  final ModuleRepository repository;
+
+  GetModuleById(this.repository);
+
+  Future<Either<Failure, Module>> call(String moduleId) {
+    return repository.getModuleById(moduleId);
+  }
+}
 
