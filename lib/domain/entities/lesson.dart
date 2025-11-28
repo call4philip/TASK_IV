155bb2386d93e12111356be36617675a2ec8af43
call4philip <philiparthur.cinco0075@gmail.com>
2025-11-28 15:43:16 +0800

    Add Lesson class with properties and Equatable support

--- lib/domain/entities/lesson.dart
+++ lib/domain/entities/lesson.dart
@@ -1,1 +1,18 @@
+import 'package:equatable/equatable.dart';
+import 'Quiz.dart';
+class Lesson extends Equatable {
+  final String id;
+  final String title;
+  final String content; // markdown/text
+  final Quiz? quiz;
 
+  const Lesson({
+    required this.id,
+    required this.title,
+    required this.content,
+    this.quiz,
+  });
+
+  @override
+  List<Object?> get props => [id, title, content, quiz];
+}
