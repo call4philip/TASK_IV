e750229e59c6273e73cb4442d968fe250ede77b2
call4philip <philiparthur.cinco0075@gmail.com>
2025-11-28 15:45:26 +0800

    Add Quiz class with equatable implementation

--- lib/domain/entities/Quiz.dart
+++ lib/domain/entities/Quiz.dart
@@ -1,1 +1,15 @@
+import 'package:equatable/equatable.dart';
+import 'Question_type.dart';
 
+class Quiz extends Equatable {
+  final String id;
+  final List<Question> questions;
+
+  const Quiz({
+    required this.id,
+    required this.questions,
+  });
+
+  @override
+  List<Object?> get props => [id, questions];
+}
